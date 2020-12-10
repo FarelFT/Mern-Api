ac552a4bb8758803a82130b53941ba5a5059feca
FarelFT <farelft02@gmail.com>
2020-12-10 15:18:52 +0700

    API Done

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const dotenv = require('dotenv');
 
 const authRoutes = require('./src/routes/authRoutes');
 const blogRoutes = require('./src/routes/blogRoutes');
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+dotenv.config({ path: './config.env' });
 const app = express();
 
 // Body-Parser
 app.use(bodyParser.json());
+
+//Middleware
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -33,4 +65,19 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(3000);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    app.listen(3000, () => console.log('Connection Success'));
+  })
+  .catch((err) => console.log(err));
